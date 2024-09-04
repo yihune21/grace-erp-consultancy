d31d4b78034e8667d1e8971600bc0e53fca71566
yihune21 <zewdieyihune@gmail.com>
2024-09-05 02:08:43 +0300

    create hero and add favicion

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import Navbar from "./components/Header/Navbar";
+import Hero from "./components/Home/Hero";
 
 function App() {
-  return <Navbar></Navbar>;
+  return <Hero />;
 }
 
 export default App;
---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,24 @@
+const Hero = () => {
+  return (
+    <>
+      <div>
+        <h2 className="">
+          GraceERP Consultancy: Your Trusted ERP Implementer and Business
+          Consultant{" "}
+        </h2>
+      </div>
+      <div>
+        <p>
+          Choosing GraceERP Consultancy for your ERP system implementation comes
+          with a range of benefits. Our experience and expertise in Odoo ERP
+          implementation are first to none, Ensuring that your business gets the
+          best possible results. Additionally, our team of experts is committed
+          to providing you with the support you need to make the most of your
+          investment in ERP Software. 
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
