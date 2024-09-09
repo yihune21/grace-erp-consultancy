f55ce87f01ad1f82162b96a8c9a366116ffc41e1
yihune21 <zewdieyihune@gmail.com>
2024-09-09 10:57:10 +0300

    make responsive

--- src/pages/ERPImplementationPage.tsx
+++ src/pages/ERPImplementationPage.tsx
@@ -5,9 +5,12 @@ const ERPImplementationPage = () => {
   return (
     <>
       <Navbar />
-      <div className="flex w-fill h-60 bg-gray-800 text-white mb-4 justify-center items-center">
-        <h1 className="text-6xl"> ERP Implementation</h1>
+      <div className="flex w-full h-60 bg-gray-800 text-white mb-4 justify-center items-center">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+          ERP Implementation
+        </h1>
       </div>
+
       <Footer />
     </>
   );
