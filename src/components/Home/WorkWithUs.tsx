a73231bc420465961f73855c62fb80817e161842
yihune21 <zewdieyihune@gmail.com>
2024-09-17 23:10:50 +0300

    update work with us component

--- src/components/Home/WorkWithUs.tsx
+++ src/components/Home/WorkWithUs.tsx
@@ -1,21 +1,15 @@
-import bgImg from "../../assets/workWithAs.avif";
+import bgImg from "../../assets/img_b.png";
 
 const links = [
   { name: "SaaS", href: "/SaaS-service" },
   { name: "Training program", href: "/training" },
   { name: "Internship program", href: "#" },
-  { name: " Outsourcing", href: "#" },
+  { name: "Outsourcing", href: "#" },
 ];
-// const stats = [
-//   { name: "Offices worldwide", value: "1" },
-//   { name: "Full-time colleagues", value: "10+" },
-//   { name: "Hours per week", value: "40" },
-//   { name: "Paid time off", value: "Unlimited" },
-// ];
 
 function WorkWithUs() {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+    <div className="relative isolate overflow-hidden bg-blue-900 py-24 sm:py-32">
       <img
         alt="bunch of people"
         src={bgImg}
@@ -30,7 +24,7 @@ function WorkWithUs() {
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#001f3f] to-[#003366] opacity-20"
         />
       </div>
       <div
@@ -42,39 +36,31 @@ function WorkWithUs() {
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#001f3f] to-[#003366] opacity-20"
         />
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          <h2 className="text-4xl font-bold tracking-tight text-[#0f172a] sm:text-6xl">
             Work with us
           </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
+          <p className="mt-6 text-lg leading-8 text-[#0f172a] ">
             Join the Future of ERP Solutions with{" "}
-            <strong>GraceERP Consultancy PLC</strong>.
+            <strong className="text-white">GraceERP Consultancy PLC</strong>.
           </p>
         </div>
         <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
             {links.map((link) => (
-              <a key={link.name} href={link.href}>
+              <a
+                key={link.name}
+                href={link.href}
+                className="hover:text-[#ea580c] transition duration-300"
+              >
                 {link.name} <span aria-hidden="true">&rarr;</span>
               </a>
             ))}
           </div>
-          {/* <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-            {stats.map((stat) => (
-              <div key={stat.name} className="flex flex-col-reverse">
-                <dt className="text-base leading-7 text-gray-300">
-                  {stat.name}
-                </dt>
-                <dd className="text-2xl font-bold leading-9 tracking-tight text-white">
-                  {stat.value}
-                </dd>
-              </div>
-            ))}
-          </dl> */}
         </div>
       </div>
     </div>
