7e5fa03dacf756a9d28f97b6276ad8cb643d1325
yihune21 <zewdieyihune@gmail.com>
2024-10-14 11:28:27 +0300

    update

--- src/components/Training/Training.tsx
+++ src/components/Training/Training.tsx
@@ -1,6 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
 
 const Training = () => {
   const photos: string[] = [
@@ -56,8 +57,8 @@ const Training = () => {
     <div className="bg-gray-50">
       {/* Title Section */}
       <div className="flex items-center justify-center">
-        <h1 className="text-gray-600 text-5xl font-bold text-center">
-          Odoo Functional Training: Empowering Ethiopia's Business Community
+        <h1 className="text-gray-600 text-5xl font-bold text-center ">
+        GraceERP Consultancy PLC recently concluded its 1st Batch Odoo Functional Training Program  
         </h1>
       </div>
 
@@ -89,7 +90,7 @@ const Training = () => {
       {/* Carousel Section */}
       <section className="max-w-7xl mx-auto py-8 px-6">
         <h3 className="text-3xl font-semibold text-center text-gray-800 mb-10">
-          Training Highlights
+         1st Batch Training Highlights
         </h3>
         <Slider {...settings}>
           {photos.map((photo, index) => (
@@ -108,19 +109,20 @@ const Training = () => {
       <section className="bg-orange-600 py-12">
         <div className="max-w-4xl mx-auto text-center">
           <h3 className="text-white text-4xl font-semibold mb-4">
-            Join Our Next Training Program
+          Join our 2nd batch training program
           </h3>
           <p className="text-white text-lg mb-8">
             Take the next step in your career and become an Odoo expert with our
             upcoming training sessions. Learn from industry leaders and unlock
             your potential.
           </p>
-          <a
-            href="#"
+          <Link
+            to="https://shorturl.at/dPGf4"
+            target="_blank"
             className="bg-white text-purple-600 font-bold py-3 px-8 rounded-full shadow-lg hover:bg-purple-50 transition-colors"
           >
-            Coming soon
-          </a>
+            Register
+          </Link>
         </div>
       </section>
     </div>
